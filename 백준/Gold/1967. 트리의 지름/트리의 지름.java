5054e2abc39b895dcb97018ef2e83b94baabf89c
HeeBeom <92363927+ShinHB417@users.noreply.github.com>
2024-02-06 14:47:12 +0900

    [Gold IV] Title: 트리의 지름, Time: 236 ms, Memory: 23836 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Node{
+    int v, w;
+    public Node(int v, int w){
+        this.v = v;
+        this.w = w;
+    }
+}
+
+public class Main {
+    static ArrayList<ArrayList<Node>> graph;
+    static int[] checked;
+    static int max = Integer.MIN_VALUE;
+
+    static int first;
+    public static void DFS(int start, int sum){
+        checked[start] = 1;
+        if(sum >= max) {
+            first = start;
+            max = sum;
+        }
+            for(Node n : graph.get(start)){
+                if(checked[n.v] == 0){
+                    checked[n.v] = 1;
+                    DFS(n.v, sum + n.w);
+                    checked[n.v] = 0;
+                }
+
+            }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        graph = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+        checked = new int[n+1];
+        for(int i = 0; i<=n; i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int i = 0; i < n-1; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(new Node(b,c));
+            graph.get(b).add(new Node(a,c));
+        }
+        DFS(1,0);
+        max = Integer.MIN_VALUE;
+        Arrays.fill(checked, 0);
+
+        DFS(first,0);
+        int answer = max;
+        System.out.println(answer);
+
+
+
+
+
+
+
+
+    }
+
+}
