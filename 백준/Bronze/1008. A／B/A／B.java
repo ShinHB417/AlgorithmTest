ffaba7db60b02e00a9b18152e799e2e215b475b2
HeeBeom <92363927+ShinHB417@users.noreply.github.com>
2024-02-07 09:54:18 +0900

    [Bronze V] Title: A/B, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double input1 = scan.nextInt();
+        double input2 = scan.nextInt();
+
+
+        System.out.println(input1 / input2);
+
+
+
+
+    }
+}
