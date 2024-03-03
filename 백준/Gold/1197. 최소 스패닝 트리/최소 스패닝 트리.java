5c2244f1e7214fe675b8429f18a70ddd36e0d2ef
HeeBeom <92363927+ShinHB417@users.noreply.github.com>
2024-03-03 17:20:00 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1660 ms, Memory: 126328 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+class Edge implements Comparable<Edge>{
+    int vex, cost;
+
+    public Edge(int vex, int cost){
+        this.vex = vex;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge e){
+        return this.cost - e.cost;
+    }
+
+
+}
+public class Main {
+
+    static ArrayList<ArrayList<Edge>> graph;
+    static int v,e,answer;
+    static boolean[] check;
+    public static void prim(){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(1,0));
+        check[1] = true;
+
+        while(!pq.isEmpty()){
+            Edge cur = pq.poll();
+            int curVex = cur.vex;
+            int curCost = cur.cost;
+            if(check[curVex] == false){
+                check[curVex] = true;
+                answer+= curCost;
+            }
+            for(Edge e : graph.get(curVex)){
+                if(check[e.vex] != true){
+                    pq.add(e);
+                }
+            }
+        }
+
+    }
+
+
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        check = new boolean[v+1];
+        graph = new ArrayList<>();
+        for(int i = 0; i <=v; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        while(e-->0){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(new Edge(b,c));
+            graph.get(b).add(new Edge(a,c));
+        }
+
+        prim();
+        System.out.println(answer);
+
+
+
+
+
+
+
+
+    }
+}
