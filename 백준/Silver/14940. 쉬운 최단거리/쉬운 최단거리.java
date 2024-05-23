ea6100e1292ad109efd02dd4443e50dbb4e077a6
HeeBeom <92363927+ShinHB417@users.noreply.github.com>
2024-05-23 13:42:12 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 812 ms, Memory: 64784 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 class Point{
     int x,y;
@@ -10,60 +13,62 @@ class Point{
 
 public class Main {
 
-    static Point dest;
+    static Point start;
     static int[] dx = {-1,0,1,0};
     static int[] dy = {0,1,0,-1};
     static int[][] board;
-    static int[][] answer;
+    static int[][] dis;
     static int row,col;
     public static void BFS(int x, int y){
         Queue<Point> q = new LinkedList<>();
-        q.add(new Point(x,y));
-        board[x][y] = 0;
+        q.add(start);
+
         while(!q.isEmpty()){
             Point cur = q.poll();
-                for(int k = 0; k < 4; k++){
-                    int nx = cur.x + dx[k];
-                    int ny = cur.y + dy[k];
+            for(int i = 0; i < 4; i++){
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
 
-                    if(nx >=0 && nx < row && ny >=0 && ny < col && board[nx][ny] == 1 ){
-                        board[nx][ny] = 0;
-                        q.add(new Point(nx,ny));
-                        answer[nx][ny] = answer[cur.x][cur.y] + 1;
-                    }
+                if(nx >=0 && nx < row && ny >=0 && ny < col && board[nx][ny] == 1){
+                    board[nx][ny] = 0;
+                    dis[nx][ny] = dis[cur.x][cur.y] + 1;
+                    q.add(new Point(nx,ny));
                 }
+            }
         }
+
+
     }
 
 
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        row = scan.nextInt();
-        col = scan.nextInt();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
         board = new int[row][col];
-        answer = new int[row][col];
+        dis = new int[row][col];
 
         for(int i = 0; i < row; i++){
+            st = new StringTokenizer(br.readLine());
             for(int j = 0; j < col; j++){
-                int a = scan.nextInt();
-                if(a == 2){
-                    dest = new Point(i,j);
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if(board[i][j] == 2){
+                    start = new Point(i,j);
                 }
-                board[i][j] = a;
             }
         }
-        answer[dest.x][dest.y] = 0;
-        BFS(dest.x, dest.y);
+        dis[start.x][start.y] = 0;
+        BFS(start.x, start.y);
 
-        for(int i = 0; i < answer.length; i++){
-            for(int j = 0; j < answer[0].length; j++){
+        for(int i = 0; i < dis.length; i++){
+            for(int j = 0; j < dis[0].length; j++){
                 if(board[i][j] == 1){
-                answer[i][j] = -1;
-            }
-                sb.append(answer[i][j] + " ");
+                    dis[i][j] = -1;
+                }
+                sb.append(dis[i][j] + " ");
             }
             sb.append("\n");
         }
@@ -71,7 +76,3 @@ public class Main {
         System.out.println(sb);
     }
 }
-
-
-
-
